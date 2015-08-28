9aee72c98183f79ea73bf1b1de8e7997ff9bbcd8
Joe VanAndel <vanandel@ucar.edu>
2015-08-28 17:16:25 +0000

    added 16 channel Analog input test - DaqAdxEx13.c

---
+++ examples/DaqAdcEx13.c
@@ -0,0 +1,135 @@
+////////////////////////////////////////////////////////////////////////////
+//
+// DaqAdcEx11.c                    I    OOO                           h
+//                                 I   O   O     t      eee    cccc   h
+//                                 I  O     O  ttttt   e   e  c       h hhh
+// -----------------------------   I  O     O    t     eeeee  c       hh   h
+// copyright: (C) 2002 by IOtech   I   O   O     t     e      c       h    h
+//    email: linux@iotech.com      I    OOO       tt    eee    cccc   h    h
+//
+////////////////////////////////////////////////////////////////////////////
+//
+// *************************************************************************
+// *                                                                       *
+// * This program is free software; you can redistribute it and/or modify  *
+// * it under the terms of the GNU General Public License as published by  *
+// * the Free Software Foundation; either version 2 of the License, or     *
+// * (at your option) any later version.                                   *
+// *                                                                       *
+// *************************************************************************
+//
+////////////////////////////////////////////////////////////////////////////
+//
+// Please email liunx@iotech with any bug reports questions or comments
+//
+////////////////////////////////////////////////////////////////////////////
+
+//--------------------------------------------------------------------------
+//
+// Single Step Acquisition Functions
+//
+//--------------------------------------------------------------------------
+
+#include <stdio.h>
+#include "daqx_linuxuser.h"
+
+///////////////////////////////////////
+//Trigger and Scan Definitions
+//
+#define STARTSOURCE	DatsImmediate   // Use Immediate Start Trigger
+#define SCANS		30              // 30 Scans 
+#define RATE		1000	        // 1000 Hz Scan Rate 
+
+
+void printConvertedData(int channels, int points, PWORD buffer, DWORD flag);
+#define MAX_CHANNELS 16
+
+int main(void)
+{
+	// DaqBoard Name and Device Handle
+	PCHAR devName;
+	DaqHandleT handle;
+
+	// Gains and Flags Array to setup the Acquisition
+	// Channels are specified in the individual commands
+	DaqAdcGain gain = DgainX1;
+	DWORD flag = DafBipolar;
+
+	// Buffer for Data Storage
+	WORD buffer[2 * SCANS];
+
+	devName = "daqBoard2k0";
+	int c;
+
+    // Open the DaqBoard2000 and get the handle
+    printf("\n\nAttempting to Connect with %s\n", devName );
+    handle = daqOpen("daqboard2k0");
+    
+    if (handle == NO_GRIP)
+    {
+        printf("Could not connect to %s\n",devName);
+        return 2;
+    }
+    printf("%s opened\n\n",devName);                        
+  
+    for (c = 0; c < MAX_CHANNELS; ++c) {
+
+	// 1 Channel 1 Point
+	daqAdcRd(handle, c, buffer, gain, flag);
+	printf("\n1 Channel %d 1 point = %d from daqAdcRd\n", c, buffer[0]);
+	printConvertedData(1, 1, buffer, flag);
+	usleep(20);
+    }
+
+    // close device connections
+     daqClose(handle);
+	printf("\n%s Closed\n\n", devName);
+
+	return 0;
+}
+
+
+VOID 
+printConvertedData(int channels, int points, PWORD buffer, DWORD flag)
+{
+	// Base Unit Voltage Parameters
+	DWORD daq10V = 1;
+	DWORD compMask = daq10V;
+	// Scaling Variables
+	float maxVolt;
+	float scale;
+	float offset;
+
+	int i, j;
+
+	//convert the data to volts:
+	//DaqBoards convert all data to an unsigned, 16-bit number (range 0 to 65535.  Zero corresponds
+	//to the minimum voltage, which is -maxVolt if in bipolar mode, or zero if unipolar.
+	//65535 corresponds to maxVolt if bipolar, or 2xmaxVolt if unipolar.  Note that a voltage
+	//higher than the device's absolute range (+/-10V for DaqBoard2000, +/-5V for other Daq* devices)
+	//can damage the device.  Setting flags and gain settings which indicate a voltage higher than
+	//the device's max, such as an unipolar, unscaled scan will result in an error before the scan
+	//is performed.
+	//
+	//The following routine automatically determines the max voltage for the device used
+	//and the proper scaling and offset factors for the polarity flags selected.
+	if (compMask & daq10V)	//Max voltage for Daq2K is +/-10V
+		maxVolt = 10.0;
+	else			//Max voltage for Legacy is +/-5V
+		maxVolt = 5.0;
+
+	//(flag&DafBiplor) equals 2 if bipolar or 0 if unipolar
+	//scale should equal maxVolt/65335 for unipolar, maxVolt/32767 for bipolar
+	scale = maxVolt / (65535 - (flag & DafBipolar) * 16384);
+	//offset should equal maxVolt for bipolar, 0 for unipolar
+	offset = maxVolt * (flag & DafBipolar) / 2;
+
+	//print scan data
+	for (i = 0; i < points; i++) {
+		for (j = 0; j < channels; j++) {
+			printf("\tChannel %d:Scan %d %1.3fV\n", j, i + 1,
+			       (float) buffer[i + j] * scale - offset);
+		}
+	}
+	return;
+}
